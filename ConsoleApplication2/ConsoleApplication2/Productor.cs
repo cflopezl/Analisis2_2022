0dfd5248d1cfa22810826311c67857f802093a5e
C-Lo 12 <39096685+cflopezl@users.noreply.github.com>
2022-08-11 22:35:26 -0600

    Productor - Consumidor RabbitMQ C#

---
+++ ConsoleApplication2/ConsoleApplication2/Productor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+class Send
+{
+    public static void Enviar(string message)
+    {
+        //1. crear la conexión al servidor
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "ColaPrueba",
+                                             durable: true,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
+                
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "ExchangeTest",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+        //Console.WriteLine(" Press [enter] to exit.");
+        //Console.ReadLine();
+    }
+
+    public static void Recibir()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "ColaPrueba",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "ColaPrueba",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+    }
+
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String continuar = "S";
+            int i = 0;
+            String json;
+
+            while (continuar == "S")
+            {
+
+                json =
+            "   [\n" +
+            "       {\n" +
+            "           \"_id\": \"5eb709f204573c1937c118de\",\n" +
+            "           \"index\": " + ++i + ",\n" +
+            "           \"name\": \"Analisis 2\"\n" +
+            "           \"timestamp\": " + DateTime.Now +",\n" +
+                "       }\n" +
+            "   ]\n";
+
+                Send.Enviar(json);
+
+                Console.WriteLine("Desea enviar otro mensaje [S/N]: ");
+                continuar = Console.ReadLine();
+            }
+
+
+            //Send.Recibir();
+        }
+            
+     }
+}
+
